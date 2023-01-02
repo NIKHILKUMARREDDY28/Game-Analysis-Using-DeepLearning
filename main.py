95a059492cff952563cd5067897e843d9ad67df6
NIKHIL KUMAR REDDY <nikhilkumarreddy28@gmail.com>
2023-01-02 19:19:29 +0530

    Added venv

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI,Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+
+@app.get("/",response_class=HTMLResponse)
+def homepage(request : Request):
+    return templates.TemplateResponse("home.html",{"request":request})
+
+
+@app.get("/RP",response_class=HTMLResponse)
+def homepage(request : Request):
+    return templates.TemplateResponse("RP.html",{"request":request})
+
+@app.get("/GO",response_class=HTMLResponse)
+def homepage(request : Request):
+    return templates.TemplateResponse("GO.html",{"request":request})
+
+@app.get("/OI",response_class=HTMLResponse)
+def homepage(request : Request):
+    return templates.TemplateResponse("OI.html",{"request":request})
+
+@app.get("/PR",response_class=HTMLResponse)
+def homepage(request : Request):
+    return templates.TemplateResponse("PR.html",{"request":request})
