13dba13f0ab62df60a445beffda48832ea05a955
NIKHIL KUMAR REDDY <nikhilkumarreddy28@gmail.com>
2023-01-02 17:32:32 +0530

    Added fastapi

---
+++ nirf/src/OI.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function OI() {
+  return (
+    <div>
+        <h1>Outreach and Inclusivity (OI)</h1>
+        <form>
+            <h4>Percentage of Students from other States/Countries (Region DiversityRD): 30 marks</h4>
+            <h4>Percentage of Women (Women Diversity WD): 30 marks</h4>
+            <h4>Economically and Socially Challenged Students (ESCS): 20 marks</h4>
+            <h4>Facilities for Physically Challenged Students (PCS): 20 marks</h4>
+
+        </form>
+    </div>
+  )
+}
+
+export default OI
---
+++ nirf/src/PR.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function PR() {
+  return (
+    <div>
+        <h1>Perception (PR)</h1>
+        <div>
+            <h4>Peer Perception: Employers & Academic Peer (PR): 100 marks</h4>
+        </div>
+    </div>
+  )
+}
+
+export default PR
